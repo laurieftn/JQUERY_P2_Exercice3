77600fe725cb89ac317a582756fcd92758aebc61
laurieftn <laurie.ftn@gmail.com>
2020-11-09 11:29:21 +0100

    Envoi de l'exercice 3, partie 2

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+$(function(){ //
+    //$('elementCiblé').css('propriété','valeur'); 
+    $("#show").click(function(){ // déclaration de la fonction qui permet d'afficher
+        $("#texte").show();
+        })
+    $("#hide").click(function(){ // déclaration de la fonction qui permet de cacher
+        $("#texte").hide();
+        })
+});
